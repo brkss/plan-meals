f6ed4bf75dbeb8fc2ae726eb0a775d40ffce2776
brkss <berkassebrahim@gmail.com>
2021-05-19 17:54:19 +0100

    web : Create Recipe Structure

--- web/src/components/Form/InputFonted.tsx
+++ web/src/components/Form/InputFonted.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import { Box } from '@chakra-ui/react';
 
 interface Props {
     type: string;
@@ -11,9 +11,14 @@ export const InputFonted : React.FC<Props> = (props) => {
 
 
     return(
-        <>
+        <Box mt={5}>
+        {
+            props.type === 'textarea' ? 
+            <TextArea placeholder={props.placeholder} rows={3} /> : 
             <Input type={props.type} placeholder={props.placeholder} />
-        </>
+        }
+            
+        </Box>
     );
 }
 
@@ -23,4 +28,15 @@ const Input = styled.input`
     font-family: 'Playfair Display', serif;
     font-weight: bold;
     font-size: 30px;
+    width: 100%;
+`;
+
+const TextArea = styled.textarea`
+    background: none;
+    border: none;
+    font-family: 'Playfair Display', serif;
+    font-weight: bold;
+    font-size: 20px;
+    resize: none;
+    width: 100%;
 `;
--- web/src/pages/recipe/CreateRecipe.tsx
+++ web/src/pages/recipe/CreateRecipe.tsx
@@ -1,21 +1,44 @@
 import React from 'react';
-import { Box, Input, Grid, GridItem, FormControl } from '@chakra-ui/react';
+import { Box, Heading } from '@chakra-ui/react';
 import { InputFonted } from '../../components/Form/InputFonted';
+import { FormControl, FormLabel, Switch, Button } from '@chakra-ui/react';
+import { ArrowForwardIcon } from '@chakra-ui/icons';
+
 
 export const CreateRecipe : React.FC = () => {
+
+    const [step, SetStep] = React.useState(1);
 
     return(
         
         <Box mt={10}>
             
-                <Box w={{md: '40%', base: '100%'}} m='auto' >
-                    
-                    <form >
 
-                        <InputFonted type='text' placeholder='Give Your Recipe A Title' />
+            <Box w={{md: '40%', base: '100%'}} m='auto' d={step !== 1 ? 'none' : 'block'} >
+                <Heading></Heading>
+                <form >
 
-                    </form>
-                </Box>
+                    <InputFonted type='text' placeholder='Give Your Recipe A Title' />
+                    <InputFonted type='textarea' placeholder='Short Description ?' />
+                    <InputFonted type='textarea' placeholder='Some tags to identify your recipe exp: low carb | high protein ...' />
+                    <FormControl display="flex" alignItems="center" mt={6}>
+                        <FormLabel  htmlFor="public-recipe" mb="0">
+                            you want this recipe to be public to everyone ?
+                        </FormLabel>
+                        <Switch id="public-recipe" colorScheme='green' />
+                    </FormControl>
+                    <FormControl mt={6}>
+                        <Button  rightIcon={<ArrowForwardIcon />} onClick={() => SetStep(2)} colorScheme="teal" variant="outline">
+                            Next
+                        </Button>
+                    </FormControl>
+                </form>
+            </Box>
+
+            <Button  rightIcon={<ArrowForwardIcon />} onClick={() => SetStep(1)} colorScheme="teal" variant="outline">
+                            Next
+                        </Button>
+                
 
         </Box>
     
