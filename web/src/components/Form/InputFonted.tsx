20445216708ba553ad0a26362efcfd85174a1455
brkss <berkassebrahim@gmail.com>
2021-05-19 18:40:38 +0100

    web: Recipe Base Information

--- web/src/components/Form/InputFonted.tsx
+++ web/src/components/Form/InputFonted.tsx
@@ -5,6 +5,8 @@ import { Box } from '@chakra-ui/react';
 interface Props {
     type: string;
     placeholder: string; 
+    id: string;
+    onChange: (e: React.FormEvent) => void
 }
 
 export const InputFonted : React.FC<Props> = (props) => {
@@ -14,8 +16,8 @@ export const InputFonted : React.FC<Props> = (props) => {
         <Box mt={5}>
         {
             props.type === 'textarea' ? 
-            <TextArea placeholder={props.placeholder} rows={3} /> : 
-            <Input type={props.type} placeholder={props.placeholder} />
+            <TextArea placeholder={props.placeholder} rows={3} id={props.id} onChange={(e ) => props.onChange(e)} /> : 
+            <Input type={props.type} placeholder={props.placeholder} id={props.id} onChange={(e) => props.onChange(e)} />
         }
             
         </Box>
@@ -39,4 +41,5 @@ const TextArea = styled.textarea`
     font-size: 20px;
     resize: none;
     width: 100%;
+    color: #5f5e5e;
 `;
--- web/src/pages/recipe/CreateRecipe.tsx
+++ web/src/pages/recipe/CreateRecipe.tsx
@@ -3,24 +3,55 @@ import { Box, Heading } from '@chakra-ui/react';
 import { InputFonted } from '../../components/Form/InputFonted';
 import { FormControl, FormLabel, Switch, Button } from '@chakra-ui/react';
 import { ArrowForwardIcon } from '@chakra-ui/icons';
+import { ErrorMessage } from '../../components/ErrorMessage';
 
 
 export const CreateRecipe : React.FC = () => {
 
     const [step, SetStep] = React.useState(1);
+    const [error, SetError] = React.useState('');
+    const [recipeInfo, SetRecipeInfo] = React.useState<any>();
+    
+    const handleRecipeInfo = (e: React.FormEvent<any>) => {
+        console.log('current element id : ', e.currentTarget.id);
+        SetRecipeInfo({
+            ...recipeInfo,
+            [e.currentTarget.id]: e.currentTarget.value
+        })
+    } 
+
+    const handleMoveStepUp = () => {
+       //check recipe info data validity 
+       if(!recipeInfo || !recipeInfo.title || !recipeInfo.tags){
+            SetError('Recipe Base Information Are Missing !');
+            return;
+       }
+       SetError('');
+       if(step < 2){
+           SetStep(step+1);
+       }
+
+    }
 
     return(
         
         <Box mt={10}>
             
-
-            <Box w={{md: '40%', base: '100%'}} m='auto' d={step !== 1 ? 'none' : 'block'} >
+            <Box w={{md: '40%', base: '100%'}} m='auto'  >
                 <Heading></Heading>
-                <form >
+                {
+                    error ? 
+                    <ErrorMessage message={error} /> : null
+                }
+                {/* STEP 1 - RECIPE BASE INFO */}
+                <form style={{display: step !== 1 ? 'none' : 'block'}}>
 
-                    <InputFonted type='text' placeholder='Give Your Recipe A Title' />
-                    <InputFonted type='textarea' placeholder='Short Description ?' />
-                    <InputFonted type='textarea' placeholder='Some tags to identify your recipe exp: low carb | high protein ...' />
+                    <InputFonted type='text' placeholder='Give Your Recipe A Title' id='title' onChange={(e) => handleRecipeInfo(e)} />
+                    <hr style={{marginTop: '12px'}} />
+                    <InputFonted type='textarea' placeholder='Short Description ?' id='description' onChange={(e) => handleRecipeInfo(e)} />
+                    <hr style={{marginTop: '12px'}} />
+                    <InputFonted type='textarea' placeholder='Some tags to identify your recipe exp: low carb | high protein ...' id='tags' onChange={(e) => handleRecipeInfo(e)} />
+                    <hr style={{marginTop: '12px'}} />
                     <FormControl display="flex" alignItems="center" mt={6}>
                         <FormLabel  htmlFor="public-recipe" mb="0">
                             you want this recipe to be public to everyone ?
@@ -28,11 +59,17 @@ export const CreateRecipe : React.FC = () => {
                         <Switch id="public-recipe" colorScheme='green' />
                     </FormControl>
                     <FormControl mt={6}>
-                        <Button  rightIcon={<ArrowForwardIcon />} onClick={() => SetStep(2)} colorScheme="teal" variant="outline">
+                        <Button  rightIcon={<ArrowForwardIcon />} onClick={() => handleMoveStepUp()} colorScheme="teal" variant="outline">
                             Next
                         </Button>
                     </FormControl>
                 </form>
+
+                {/* STEP 2 - INGREDIENTS */}
+                <Box d={step !== 2 ? 'none' : 'block'}>
+                    Ingredients
+                </Box>
+                
             </Box>
 
             <Button  rightIcon={<ArrowForwardIcon />} onClick={() => SetStep(1)} colorScheme="teal" variant="outline">
