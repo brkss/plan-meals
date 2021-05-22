49158a1c8400fd83166cd1799976059a30afe63f
brkss <berkassebrahim@gmail.com>
2021-05-22 15:27:01 +0100

    web: Recipe info

--- web/src/components/ErrorMessage.tsx
+++ web/src/components/ErrorMessage.tsx
@@ -3,12 +3,13 @@ import { Box, Alert, AlertIcon, AlertDescription } from '@chakra-ui/react';
 
 interface Props {
     message: string;
+    w?: any;
 }
 
-export const ErrorMessage : React.FC<Props> = ({message}) => {
+export const ErrorMessage : React.FC<Props> = ({message, w}) => {
 
     return(
-        <Box my={4}>
+        <Box my={4}  w={w}>
             <Alert status="error" borderRadius={4}>
                 <AlertIcon />
                 <AlertDescription>{message}</AlertDescription>
--- web/src/pages/recipe/InfoRecipe.tsx
+++ web/src/pages/recipe/InfoRecipe.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { RouteComponentProps } from 'react-router';
-import {Box, Heading} from '@chakra-ui/react';
+import {Box, Heading, Text, Grid, GridItem } from '@chakra-ui/react';
 import axios from '../../config/axios';
 import { URLS } from '../../helpers/Constants';
+import { ErrorMessage } from '../../components/ErrorMessage';
 
-export const InfoRecipe : React.FC<RouteComponentProps<any>> = ({match}) => {
+export const InfoRecipe : React.FC<RouteComponentProps<any>> = ({match, history}) => {
 
     const [loading, SetLoading] = React.useState(false);
-    const [error, SetError] = React.useState<any>();
+    const [error, SetError] = React.useState<any>('');
     const [recipe, SetRecipe] = React.useState<any>();
 
 
@@ -17,7 +18,8 @@ export const InfoRecipe : React.FC<RouteComponentProps<any>> = ({match}) => {
             SetLoading(false);
             const _data = resp.data;
             if(_data.status === false){
-                SetError(_data.message);
+                if(_data.message.includes('not found')) history.push('/dash/recipe/list');
+                else SetError(_data.message);
                 return;
             }else if(_data.status === true){
                 SetRecipe(_data.data[0]);
@@ -27,10 +29,33 @@ export const InfoRecipe : React.FC<RouteComponentProps<any>> = ({match}) => {
     }, []);
 
     if(loading || !recipe) return <>Loading</>
+    if(error) return <Box mt={7}> <ErrorMessage message={error} w={{md: '40%', base: '100%'}} /> </Box>
 
     return(
-        <Box mt={7}>
+        <Box mt={7} w={{md: '50%', base: '100%'}} m='auto'>
             <Heading> {recipe.title} </Heading>
+            <Text opacity={.8}>
+                {recipe.description}
+            </Text>
+
+            <Box bg='blue.50' rounded={6} padding={4} mt={5}>
+                <Heading fontSize='20px'> Ingredients </Heading>
+                <Grid templateColumns="repeat(3, 1fr)" gap={0}>
+                    {
+                        recipe.ingredients.map((ing: any, key: any) => (
+                            <GridItem colSpan={{md: 1, base: 3}}  >
+                                <Text p={3} mt={4} mr={3} bg='blue.100' rounded={6}>
+                                    <Text fontWeight='bold'>{ing.measurement}</Text>
+                                    <Text>{ing.grocery.title}</Text>
+                                </Text>
+                            </GridItem>
+                        )) 
+                    }
+
+                    
+                </Grid>
+            </Box>
+
         </Box>
     );
 }
