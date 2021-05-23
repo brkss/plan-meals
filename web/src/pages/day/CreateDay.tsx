aaac2a5ad1ddf0303170c6507023f2e589d32362
brkss <berkassebrahim@gmail.com>
2021-05-23 23:36:20 +0100

    web: fix date parsing

--- web/src/pages/day/CreateDay.tsx
+++ web/src/pages/day/CreateDay.tsx
@@ -1,10 +1,21 @@
+import { Box, Heading, FormControl } from '@chakra-ui/react';
 import React from 'react';
-
+import { InputFonted } from '../../components/Form/InputFonted';
 
 export const CreateDay : React.FC = () => {
 
 
     return(
-        <>create day</>
+        <Box mt={5}>
+            <Heading>Create Day</Heading>
+            <form >
+                <FormControl>
+                    <InputFonted type='text' id="title" onChange={() => {}} placeholder="Title exp. Monday"  />
+                </FormControl>
+                <FormControl>
+                    <InputFonted type='datetime' id="title" onChange={() => {}} placeholder="Title exp. Monday"  />
+                </FormControl>
+            </form>
+        </Box>
     )
 }
