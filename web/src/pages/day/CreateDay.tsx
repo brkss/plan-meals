bc89731eb31847d1cf40409f4e38987f7742c847
brkss <berkassebrahim@gmail.com>
2021-05-24 23:30:31 +0100

    web: setup handler to creare date

--- web/src/pages/day/CreateDay.tsx
+++ web/src/pages/day/CreateDay.tsx
@@ -1,19 +1,40 @@
-import { Box, Heading, FormControl } from '@chakra-ui/react';
+import { Box, Heading, FormControl, Button } from '@chakra-ui/react';
 import React from 'react';
 import { InputFonted } from '../../components/Form/InputFonted';
 
 export const CreateDay : React.FC = () => {
 
+    const [loading, SetLoading] = React.useState(false);
+    const [form, SetForm] = React.useState<any>();
+    
+    // 
+    const handleForm = (e: React.FormEvent<any>) => {
+        SetForm({
+            ...form,
+            [e.currentTarget.id]: e.currentTarget.value
+        })
+    }
+
+    //
+    const handleDayCreation = () => {
+        
+    }
+
 
     return(
         <Box mt={5}>
-            <Heading>Create Day</Heading>
+            <Heading>Days</Heading>
             <form >
                 <FormControl>
-                    <InputFonted type='text' id="title" onChange={() => {}} placeholder="Title exp. Monday"  />
+                    <InputFonted type='text' id="title" onChange={(e) => handleForm(e)} placeholder="Title exp. Monday"  />
                 </FormControl>
                 <FormControl>
-                    <InputFonted type='datetime' id="title" onChange={() => {}} placeholder="Title exp. Monday"  />
+                    <InputFonted type='date' id="date" onChange={(e) => handleForm(e)} placeholder="Title exp. Monday"  />
+                </FormControl>
+                <FormControl>
+                <Button colorScheme="teal" variant="outline">
+                    Create
+                </Button>
                 </FormControl>
             </form>
         </Box>
