bb89228a0c87aa761d23435fb35682912431acd8
brkss <berkassebrahim@gmail.com>
2021-05-24 23:34:33 +0100

    web: create day validation

--- web/src/pages/day/CreateDay.tsx
+++ web/src/pages/day/CreateDay.tsx
@@ -1,10 +1,12 @@
 import { Box, Heading, FormControl, Button } from '@chakra-ui/react';
 import React from 'react';
+import { ErrorMessage } from '../../components/ErrorMessage';
 import { InputFonted } from '../../components/Form/InputFonted';
 
 export const CreateDay : React.FC = () => {
 
     const [loading, SetLoading] = React.useState(false);
+    const [error, SetError] = React.useState('');
     const [form, SetForm] = React.useState<any>();
     
     // 
@@ -17,13 +19,21 @@ export const CreateDay : React.FC = () => {
 
     //
     const handleDayCreation = () => {
-        
+        if(!form || !form.title || !form.date){
+            SetError('Please check your data!');
+            return;
+        }
+        SetError('');
     }
 
 
     return(
         <Box mt={5}>
             <Heading>Days</Heading>
+            {
+                error ? 
+                <ErrorMessage message={error} /> : null
+            }
             <form >
                 <FormControl>
                     <InputFonted type='text' id="title" onChange={(e) => handleForm(e)} placeholder="Title exp. Monday"  />
@@ -32,7 +42,7 @@ export const CreateDay : React.FC = () => {
                     <InputFonted type='date' id="date" onChange={(e) => handleForm(e)} placeholder="Title exp. Monday"  />
                 </FormControl>
                 <FormControl>
-                <Button colorScheme="teal" variant="outline">
+                <Button colorScheme="teal" variant="outline" onClick={() => handleDayCreation()} >
                     Create
                 </Button>
                 </FormControl>
