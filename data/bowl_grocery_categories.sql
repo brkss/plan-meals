fe07717ce6d4f464abf8f79ce5f51a0d0f01fe11
brkss <berkassebrahim@gmail.com>
2021-06-03 12:14:46 +0100

    📃 Data

---
+++ data/bowl_grocery_categories.sql
@@ -0,0 +1,8 @@
+INSERT INTO bowl_grocery_category(title) VALUES 
+('Start with a base'),
+('Add your veggies'),
+('Fruity fun'),
+('Pack your protein'),
+('Healthy fat'),
+('Complex Carbohydrates'),
+('A little something extra');
