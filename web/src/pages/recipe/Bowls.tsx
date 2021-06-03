8487fc7825842e934ae3b573196d2eda50a35e36
brkss <berkassebrahim@gmail.com>
2021-06-03 17:42:48 +0100

    🥑 map thru bowls

--- web/src/pages/recipe/Bowls.tsx
+++ web/src/pages/recipe/Bowls.tsx
@@ -10,8 +10,20 @@ import { BsEye, BsPencil, BsTrash } from 'react-icons/bs';
 export const ListBowls : React.FC<RouteComponentProps> = ({history}) => {
 
     const [loading, Setloading] = React.useState(false);
+    const [bowls, SetBowls] = React.useState<any []>();
 
+    React.useEffect(() => {
+        axios.get(URLS.bowl.bowls).then(res => {
+            console.log('res => ', res);
+            const _data = res.data;
+            if(_data.status === true){
+                console.log('coted => ', _data.data);
+                SetBowls(_data.data);
+            }
+        });
+    }, []);
     
+
     const handleCropText = (text: string) => {
         return text.length > 127 ? 
             `${text.substring(0, 127)}...`:
@@ -27,19 +39,24 @@ export const ListBowls : React.FC<RouteComponentProps> = ({history}) => {
         <Box mt={5}>
             <Heading>My Bowls</Heading>
             <Grid templateColumns="repeat(3, 1fr)" gap={0}>
-                <GridItem colSpan={{md: 1, base: 3}}  >
-                    <Box p={4} mt={4} mr={3}  bg='gray.100' rounded={6} minH='140px' >
-                            <Text fontWeight='bold'>Bowl Title</Text>
-                            <Text fontSize={11} minHeight={'44px'} opacity={.8}>{handleCropText("Simple bowl ticked description.")}</Text>
-                            <ButtonGroup size="sm" isAttached variant="outline" mt={2} borderColor='gray.900'>
-                                <Button bg='gray.200' mr="-px" leftIcon={<BsEye />} onClick={() => {
-                                    history.push(`/dash/recipe/info/1`)
-                                }}>View</Button>
-                                <IconButton bg='gray.200' aria-label="Delete Recipe" icon={<BsTrash />} isLoading={true} onClick={() => {}} />
-                                <IconButton bg='gray.200' aria-label="Edit Recipe" icon={<BsPencil />} />
-                            </ButtonGroup> 
-                    </Box>    
-                </GridItem>
+                {
+                    bowls?.map((bowl, key) => (
+                        <GridItem key={key} colSpan={{md: 1, base: 3}}  >
+                            <Box p={4} mt={4} mr={3}  bg='gray.100' rounded={6} minH='140px' >
+                                    <Text fontWeight='bold'>{bowl.title}</Text>
+                                    <Text fontSize={11} minHeight={'44px'} opacity={.8}>{handleCropText(bowl.ticket)}</Text>
+                                    <ButtonGroup size="sm" isAttached variant="outline" mt={2} borderColor='gray.900'>
+                                        <Button bg='gray.200' mr="-px" leftIcon={<BsEye />} onClick={() => {
+                                            history.push(`/dash/recipe/info/1`)
+                                        }}>View</Button>
+                                        <IconButton bg='gray.200' aria-label="Delete Recipe" icon={<BsTrash />} isLoading={true} onClick={() => {}} />
+                                        <IconButton bg='gray.200' aria-label="Edit Recipe" icon={<BsPencil />} />
+                                    </ButtonGroup> 
+                            </Box>    
+                        </GridItem>
+                    ))
+                }
+                
                    
                 
                 <GridItem colSpan={{md: 1, base: 3}}  >
