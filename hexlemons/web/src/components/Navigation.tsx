035f6f52506dd5c9e900eae48ec81468afbe7ffd
brkss <berkassebrahim@gmail.com>
2021-06-05 12:40:34 +0100

    🥑 Setup dash navigation UI

---
+++ hexlemons/web/src/components/Navigation.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Box, Center, Tooltip } from '@chakra-ui/react';
+import * as ReactIcon from 'react-icons/all';
+import { useHistory } from 'react-router';
+import { ICONS } from './icons/icons_data';
+import { IconCupr } from './icons/icon';
+
+export const Navigation : React.FC = () => {
+
+    const history = useHistory();
+    const redir = (path: string) => {
+        history.push(path);
+    }
+
+    return(
+        <>
+            <Box position={{md: 'fixed', base: 'initial'}} >
+
+                <Tooltip label="Home" placement="right">
+                    <Center onClick={_ => redir('/dash/grocery')} w="40px" h="40px" bg='#429789' p={2} rounded={6} m='auto'  mb={3} mt={3} cursor='pointer' _hover={{bg: '#4cb1a0'}} transition='.3s'>
+                        <ReactIcon.BiHomeAlt  style={{fontSize: '25px'}} />
+                    </Center>
+                </Tooltip>
+
+                <Tooltip label="Bowls" placement="right">
+                    <Center onClick={_ => redir('/dash/recipe')}  w="40px" h="40px" bg='#429789' p={2} rounded={6} m='auto' mb={3} color='#053029' cursor='pointer' _hover={{bg: '#4cb1a0'}} transition='.3s'>
+                        <ReactIcon.CgBowl style={{fontSize: '25px'}} />
+                    </Center>
+                </Tooltip>
+                
+                <Tooltip label="Recipes" placement="right">
+                    <Center onClick={_ => redir('/dash/recipe')}  w="40px" h="40px" bg='#429789' p={2} rounded={6} m='auto' mb={3} color='#053029' cursor='pointer' _hover={{bg: '#4cb1a0'}} transition='.3s'>
+                        <ReactIcon.BiDish style={{fontSize: '25px'}} />
+                    </Center>
+                </Tooltip>
+
+                <Tooltip label="Days & Meals" placement="right">
+                    <Center onClick={_ => redir('/dash/day')} w="40px" h="40px" bg='#429789' p={2} rounded={6} m='auto' mb={3} color='#053029' cursor='pointer' _hover={{bg: '#4cb1a0'}} transition='.3s'>
+                        <ReactIcon.BiCalendar style={{fontSize: '25px'}} />
+                    </Center>
+                </Tooltip>
+
+                <Tooltip label="Settings" placement="right">
+                    <Center onClick={_ => redir('/dash/settings')} w="40px" h="40px" bg='#429789' p={2} rounded={6} m='auto' mb={3} color='#053029' cursor='pointer' _hover={{bg: '#4cb1a0'}} transition='.3s'>
+                        <ReactIcon.BiCog style={{fontSize: '25px'}} />
+                    </Center>
+                </Tooltip>
+
+                </Box>
+        </>
+    )
+}
--- hexlemons/web/src/pages/dashboard/dashboard.tsx
+++ hexlemons/web/src/pages/dashboard/dashboard.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Grid, GridItem } from '@chakra-ui/react';
+import { Navigation } from '../../components/Navigation';
 
 export const Dashboard : React.FC = () => {
 
     return(
         <>
             
-            <Grid templateColumns="repeat(23, 1fr)" height='100vh' >
-                <GridItem colSpan={1} bg="#ACCDC5" borderRight='1px solid #0000002b'>
-                
+            <Grid templateColumns="repeat(36, 1fr)" height='100vh' >
+                <GridItem colSpan={2} bg="#ACCDC5" borderRight='1px solid #0000002b' p={5}>
+                    <Navigation />
                 </GridItem>
-                <GridItem colSpan={22} bg="#F4F3E7" >
+                <GridItem colSpan={34} bg="#F4F3E7" >
                 
                 </GridItem>
             </Grid>
