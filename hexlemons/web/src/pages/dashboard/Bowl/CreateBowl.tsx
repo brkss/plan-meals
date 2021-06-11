2a441ad8dadb9551ebe8b4bd526cbe86b2440922
brkss <berkassebrahim@gmail.com>
2021-06-11 13:18:12 +0100

    ✨ MAP ELEMENT AND CATEGORIES TO CREATE BOWL

--- hexlemons/web/src/pages/dashboard/Bowl/CreateBowl.tsx
+++ hexlemons/web/src/pages/dashboard/Bowl/CreateBowl.tsx
@@ -10,14 +10,34 @@ import { useBowlElementCategoriesQuery, useBowlElementsWithCategoryQuery } from 
 
 export const CreateBowl : React.FC = () => {
 
-    React.useEffect(() => {
-        // map element in categories 
-    }, []);
-    
     const { isOpen, onOpen, onClose } = useDisclosure();
     const elements = useBowlElementsWithCategoryQuery(); 
     const categories = useBowlElementCategoriesQuery();
-    const [mappedElements, SetMappedElements] = React.useState<any []>([]);
+    const [mappedElements, SetMappedElements] = React.useState<any[]>([]);
+
+    React.useEffect(() => {
+        // map element in categories 
+        elements.data?.bowlElementWithCateogry.map((element, key) => {
+            const index = mappedElements.findIndex(x => x.id === element.category.id);
+            if(index === -1){
+                // element not found 
+                mappedElements.push({
+                    ...element.category,
+                    elements: [
+                        element
+                    ]
+                });
+            }else {
+                // element found
+                mappedElements[index].elements.push(element);
+            }
+            SetMappedElements(mappedElements);
+        });
+        
+    }, [mappedElements]);
+    
+    
+    
 
     
 
@@ -41,79 +61,24 @@ export const CreateBowl : React.FC = () => {
                             {category.description} 
                             <ButtonAddElement text="CLICK HERE TO CREATE ELEMENTS ?" onClick={() => onOpen()} />
                         </Text>
+                        <Box>
+                            <Grid  templateColumns="repeat(21, 1fr)" gap={5} pt={5} mt={4}>
+                                    {
+                                        elements.data?.bowlElementWithCateogry.map((element, key) => (
+                                            element.category.id === category.id ? 
+                                                <GridItem key={key} colSpan={{md: 3, base: 7}}>
+                                                    <BowlElement title={element.title} calories={element.calories} image={element.image} />
+                                                </GridItem>
+                                            : null
+                                        ))
+                                    }
+                            </Grid>
+                        </Box>
                     </Box>
                 ))
             }
             
-            <Text fontSize='25px' fontWeight='900' textTransform='uppercase' color='#383737'>
-                choose a base 
-                <ButtonAddElement text="CLICK HERE TO CREATE ELEMENTS ?" onClick={() => onOpen()} />
-            </Text>
             
-            <Box w={{md: '30%', base: '100%'}} mt={3} d='none'>
-                <InputRegular type='text' placeholder='Search'  />
-            </Box>
-            <Box>
-                <Grid  templateColumns="repeat(21, 1fr)" gap={5} pt={5} mt={4}>
-                    <GridItem colSpan={{md: 3, base: 7}}>
-                        <BowlElement />
-                    </GridItem>
-                    <GridItem colSpan={{md: 3, base: 7}}>
-                        <BowlElement />
-                    </GridItem>
-                    <GridItem colSpan={{md: 3, base: 7}}>
-                        <BowlElement />
-                    </GridItem>
-                    <GridItem colSpan={{md: 3, base: 7}} >
-                        <BowlElement />
-                    </GridItem>
-                    <GridItem colSpan={{md: 3, base: 7}}>
-                        <BowlElement />
-                    </GridItem>
-                    <GridItem colSpan={{md: 3, base: 7}}>
-                        <BowlElement />
-                    </GridItem>
-                    <GridItem colSpan={{md: 3, base: 7}} >
-                        <BowlElement />
-                    </GridItem>
-                    <GridItem colSpan={{md: 3, base: 7}} >
-                        <BowlElement />
-                    </GridItem>
-                    <GridItem colSpan={{md: 3, base: 7}} >
-                        <BowlElement />
-                    </GridItem>
-                    <GridItem colSpan={{md: 3, base: 7}} >
-                        <BowlElement />
-                    </GridItem>
-                    <GridItem colSpan={{md: 3, base: 7}} >
-                        <BowlElement />
-                    </GridItem>
-                    <GridItem colSpan={{md: 3, base: 7}} >
-                        <BowlElement />
-                    </GridItem>
-                    <GridItem colSpan={{md: 3, base: 7}} >
-                        <BowlElement />
-                    </GridItem>
-                    <GridItem colSpan={{md: 3, base: 7}} >
-                        <BowlElement />
-                    </GridItem>
-                    <GridItem colSpan={{md: 3, base: 7}} >
-                        <BowlElement />
-                    </GridItem>
-                    <GridItem colSpan={{md: 3, base: 7}} >
-                        <BowlElement />
-                    </GridItem>
-                    <GridItem colSpan={{md: 3, base: 7}} >
-                        <BowlElement />
-                    </GridItem>
-                    <GridItem colSpan={{md: 3, base: 7}} >
-                        <BowlElement />
-                    </GridItem>
-                    <GridItem colSpan={{md: 3, base: 7}} >
-                        <BowlElement />
-                    </GridItem>
-                </Grid>
-            </Box>
             <CreateElement isOpen={isOpen} onClose={onClose} onOpen={onOpen} />
         </Box>
     );  
